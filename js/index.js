2f24b43dc34bcbe13ac20f17ea38b03ce225e013
Thomas Richter <thomas.karl.richter@gmail.com>
2014-05-15 10:24:40 -0400

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
+/* globals $ */
+"use strict";
+
 $(document).ready(function(){
+
+    var mytracker;
     
     var geoarray = [];
     var lat, long, twoPointsDis;
     var last_pos, cur_pos;
     var totalDis = 0; 
+    
     function calculateDistance(lat1, lon1, lat2, lon2) {
         var R = 6371; // km
         var dLat = (lat2-lat1).toRad();
@@ -18,50 +24,47 @@ $(document).ready(function(){
     Number.prototype.toRad = function() {
         return this * Math.PI / 180;
     };
+    function getPosition(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        if(geoarray.length > 0){
+           last_pos = geoarray[geoarray.length - 1];
+        } else {
+           last_pos = {lat: lat, long: long};
+        }
+        twoPointsDis = calculateDistance(lat, long,
+                                         last_pos.lat, last_pos.long);
+        
+        cur_pos = {lat: lat, long: long, dis: twoPointsDis};                               
+        geoarray.push(cur_pos);
+        
+        for (var i = 0; i < geoarray.length; i++) {
+           totalDis = totalDis + geoarray[i].dis;
+        }
+        $('#cur_pos').append("<div><i>"+cur_pos.lat +
+                            "</i>: <i>"+cur_pos.long+
+                            "</i> Dist: <b>"+twoPointsDis+
+                            "</b></i> Total Distance: <b>"+totalDis+"</b></div>");
+    };
+    
+    function run(){
+        navigator.geolocation.getCurrentPosition(getPosition);
+    };
+    
+    function start(){
+        mytracker  = setInterval(run, 2000);
+    };
+    
+    function stopp(){
+        window.clearInterval(mytracker);
+    };
     
     if (navigator.geolocation) {
-        function getPosition(position) {
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
-            if(geoarray.length > 0){
-               last_pos = geoarray[geoarray.length - 1];
-            } else {
-               last_pos = {lat: lat, long: long};
-            }
-            twoPointsDis = calculateDistance(lat, long,
-                                             last_pos.lat, last_pos.long);
-            
-            cur_pos = {lat: lat, long: long, dis: twoPointsDis};                               
-            geoarray.push(cur_pos);
-            
-           // console.log(geoarray.length);
-           for (var i = 0; i < geoarray.length; i++) {
-               totalDis = totalDis + geoarray[i].dis;
-           }
-           $('#cur_pos').append("<div><i>"+cur_pos.lat +"</i>: <i>"+cur_pos.long+"</i> Dist: <b>"+twoPointsDis+"</b></div>" +
-           "</i> Total Distance: <b>"+totalDis+"</b></div>");
-
-        };
-        
-        function run(){
-            navigator.geolocation.getCurrentPosition(getPosition);
-        };
-        
-        var mytracker;
-        
-        function start(){
-            mytracker  = setInterval(run, 2000);
-        };
-        
-        function stopp(){
-            window.clearInterval(mytracker);
-        };
-        
         $("#start").on("click", start);
         $("#stopp").on("click", stopp);
     }
     else {
-      console.log('Geolocation is not supported for this Browser/OS version yet.');
+        console.log('Geolocation is not supported for this Browser/OS version yet.');
     }
     
 });
